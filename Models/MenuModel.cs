24abedf5a3c8af381956fcffd4994d047efe25a9
Carl Olsson <calle.olsson@hotmail.com>
2020-01-26 17:11:34 +0100

    Add project files.

---
+++ Models/MenuModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PoeDashboard.Web.Models
+{
+    public class MenuModel
+    {
+        public MenuModel(string clientIp)
+        {
+            ClientIp = clientIp;
+        }
+        public string ClientIp { get; set; }
+    }
+}
