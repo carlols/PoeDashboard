b99f0b5808dd3e15482e0e068c05cfc9ca7e2335
Carl Olsson <calle.olsson@hotmail.com>
2020-01-27 22:33:23 +0100

    Implemented fractioned currency value popover for Exalted Orb.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,13 @@ namespace PoeDashboard.Web.Controllers
             return View();
         }
 
+        public IActionResult LoadCurrencyPopover(string id, int valueInExalt, string currencyName)
+        {
+            var model = new CurrencyPopoverModel(id, valueInExalt, currencyName);
+
+            return View("_CurrencyPopover", model);
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/CurrencyPopoverModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoeDashboard.Web.Models
+{
+    public class CurrencyPopoverModel
+    {
+        public CurrencyPopoverModel(string elementId, int valueInExalt, string currencyName)
+        {
+            ElementId = elementId;
+            ValueInExalt = valueInExalt;
+            CurrencyName = currencyName;
+            FractionalValues = CalculateFractionalValues(valueInExalt);
+        }
+        public string ElementId { get; set; }
+        public int ValueInExalt { get; set; }
+        public string CurrencyName { get; set; }
+
+        public List<(double fraction, int value)> FractionalValues { get; }
+
+        private List<(double fraction, int value)> CalculateFractionalValues(int valuesInExalt)
+        {
+            List<(double fraction, int value)> tempDic = new List<(double fraction, int value)>();
+
+            for (var i = 1; i < 10; i++)
+            {
+                double divided = (double)i / 10;
+                tempDic.Add((divided, (int)Math.Ceiling(valuesInExalt * divided)));
+            }
+
+            return tempDic;
+        }
+    }
+}
