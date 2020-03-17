edefd2cea731b805d0df16680f34f4ac38e31cee
Carl Olsson <calle.olsson@hotmail.com>
2020-03-17 15:53:19 +0100

    Add configurable currency tracking, letting users select what to see.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,28 @@
 ﻿$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+String.prototype.replaceAll = function (s1, s2) {
+    return this.replace(
+        new RegExp(s1.replace(/[.^$*+?()[{\|]/g, '\\$&'), 'g'),
+        s2
+    );
+};
+
+String.prototype.toCamelCase = function () {
+    var trimmedString = this.replace(/[^A-Z0-9]/ig, "");
+    return trimmedString[0].toLowerCase() + trimmedString.substring(1);
+};  
+
+var removeValueFromCommaSeparatedString = function (list, value, separator) {
+    console.log(list);
+    separator = separator || ",";
+    var values = list.split(separator);
+    for (var i = 0; i < values.length; i++) {
+        if (values[i] == value) {
+            values.splice(i, 1);
+            return values.join(separator);
+        }
+    }
+    return list;
+}
