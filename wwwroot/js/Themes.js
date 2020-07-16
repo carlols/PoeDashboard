2541a7a0333efde89703e57d1b5b2fba6e787a84
Carl Olsson <calle.olsson@hotmail.com>
2020-07-16 02:20:51 +0200

    Add a footer with github info. + Implement theme selection framework and add Dark/Light themes.

---
+++ wwwroot/js/Themes.js
@@ -0,0 +1,30 @@
+﻿const defaultTheme = "dark";
+const themeKey = "poeDashboardTheme";
+
+function ApplyTheme() {
+    var theme = localStorage.getItem(themeKey);
+
+    if (!theme) {
+        theme = defaultTheme;
+    }
+
+    trans();
+    $("html").attr("data-theme", theme);
+}
+
+function SaveThemeToLocalStorage(theme) {
+    if (!theme) {
+        theme = defaultTheme;
+    }
+
+    localStorage.setItem(themeKey, theme);
+    ApplyTheme();
+}
+
+let trans = () => {
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => {
+        document.documentElement.classList.remove("transition");
+    },
+        350);
+}
